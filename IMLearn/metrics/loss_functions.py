path fix for run_perceptron and loss calculation fix for misclassification

IMLearn/metrics/loss_functions.py
@@ -37,7 +37,8 @@ def misclassification_error(y_true: np.ndarray, y_pred: np.ndarray, normalize: b
     Misclassification of given predictions
     """
     res = y_true * y_pred
-    loss = np.sum(res[res<0])
+    loss_idxs = res[res<0]
+    loss = loss_idxs.size
     if normalize:
         loss = loss / y_true.size
     return loss

exercises/classifiers_evaluation.py
@@ -38,9 +38,10 @@ def run_perceptron():
     """
     for n, f in [("Linearly Separable", "linearly_separable.npy"), ("Linearly Inseparable", "linearly_inseparable.npy")]:
         # Load dataset
-        X, y = load_dataset(f)
+        X, y = load_dataset("datasets/" + f)
 
         # Fit Perceptron and record loss in each fit iteration
+        
         losses = []
         def loss_record_callback(fit: Perceptron, x: np.ndarray, y: int):
             losses.append(fit.loss(X, y))