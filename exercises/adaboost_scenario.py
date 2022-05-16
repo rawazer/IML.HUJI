1b66ad6ec1f0850255aebe8c7e5a78a52d21f797
Roi Reiter <roireit@gmail.com>
2022-05-16 09:26:25 +0300

    implemented adaboost_scenario

--- exercises/adaboost_scenario.py
+++ exercises/adaboost_scenario.py
@@ -1,6 +1,6 @@
 import numpy as np
 from typing import Tuple
-from IMLearn.learners.metalearners.adaboost import AdaBoost
+from IMLearn.metalearners.adaboost import AdaBoost
 from IMLearn.learners.classifiers import DecisionStump
 from utils import *
 import plotly.graph_objects as go
@@ -42,20 +42,77 @@ def fit_and_evaluate_adaboost(noise, n_learners=250, train_size=5000, test_size=
     (train_X, train_y), (test_X, test_y) = generate_data(train_size, noise), generate_data(test_size, noise)
 
     # Question 1: Train- and test errors of AdaBoost in noiseless case
-    raise NotImplementedError()
+    ada = AdaBoost(DecisionStump, n_learners)
+    ada.fit(train_X, train_y)
+    test_err, train_err = [], []
+    for t in range(1, n_learners, 1):
+        train_err.append(ada.partial_loss(train_X, train_y, t))
+        test_err.append(ada.partial_loss(test_X, test_y, t))
+    
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=np.linspace(1, n_learners, n_learners), y=train_err, 
+                        mode='lines', name='train'))
+    fig.add_trace(go.Scatter(x=np.linspace(1, n_learners, n_learners), y=test_err, 
+                        mode='lines', name='test'))
+    fig.update_layout(title="Loss of AdaBoost against train and test for number of models",
+            xaxis_title="Number of models used",
+            yaxis_title="Loss")
+    fig.show()
 
     # Question 2: Plotting decision surfaces
     T = [5, 50, 100, 250]
     lims = np.array([np.r_[train_X, test_X].min(axis=0), np.r_[train_X, test_X].max(axis=0)]).T + np.array([-.1, .1])
-    raise NotImplementedError()
+
+    symbols_test = test_y.astype(str)
+    symbols_test[symbols_test == '1.0'] = 'cross-thin'
+    symbols_test[symbols_test == '-1.0'] = 'line-ew'
+    for t in T:
+        def cur_pred(X):
+            return ada.partial_predict(X, t)
+        fig = go.Figure()
+        fig.add_trace(decision_surface(cur_pred, lims[0, :], lims[1, :]))
+        fig.add_trace(go.Scatter(x=test_X[:, 0], y=test_X[:, 1],
+            mode='markers', marker=dict(
+            symbol=symbols_test,
+            size=np.abs(test_y) * 3
+        )))
+        fig.update_layout(title="Decision surfaces of AdaBoost using " + str(t) + " models")
+        fig.show()
 
     # Question 3: Decision surface of best performing ensemble
-    raise NotImplementedError()
+    best_t = np.argmin(test_err) + 1
+    def cur_pred(X):
+        return ada.partial_predict(X, best_t)
+    fig = go.Figure()
+    fig.add_trace(decision_surface(cur_pred, lims[0, :], lims[1, :]))
+    fig.add_trace(go.Scatter(x=test_X[:, 0], y=test_X[:, 1],
+            mode='markers', marker=dict(
+                symbol=symbols_test,
+                size=np.abs(test_y) * 3
+            )))
+    fig.update_layout(title="Decision surfaces of AdaBoost using best amount of models = " + str(best_t) + ",\nGot an error of " + str(test_err[best_t - 1]))
+    fig.show()
 
     # Question 4: Decision surface with weighted samples
-    raise NotImplementedError()
+    def cur_pred(X):
+        return ada.partial_predict(X, n_learners)
+    D = (ada.D_ / np.max(ada.D_)) * 10
+    symbols_train = train_y.astype(str)
+    symbols_train[symbols_train == '1.0'] = 'cross-thin'
+    symbols_train[symbols_train == '-1.0'] = 'line-ew'
+    fig = go.Figure()
+    fig.add_trace(decision_surface(cur_pred, lims[0, :], lims[1, :]))
+    fig.add_trace(go.Scatter(x=train_X[:, 0], y=train_X[:, 1],
+                mode='markers', marker=dict(
+                    symbol=symbols_train,
+                    size=D
+                )))
+    fig.update_layout(title="Decisions of last ensemble")
+    fig.show()
 
 
 if __name__ == '__main__':
     np.random.seed(0)
-    raise NotImplementedError()
+    fit_and_evaluate_adaboost(0, 250)
+    fit_and_evaluate_adaboost(0.4, 250)
+
